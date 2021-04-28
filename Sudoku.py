13dde9d8dfa6782e0db53c31c40d1eb4aebe231f
Iam <78688956+Indhu-Sree-S@users.noreply.github.com>
2021-04-28 10:13:43 +0530

    Add files via upload

---
+++ Sudoku.py
@@ -0,0 +1,93 @@
+def print_board(board):
+    '''Prints the board'''
+    boardString = ""
+    for i in range(9):
+        for j in range(9):
+            boardString += str(board[i][j]) + " "
+            if (j+1)%3 == 0 and j != 0 and (j+1) != 9:
+                boardString += "| "
+
+            if j == 8:
+                boardString += "\n"
+
+            if j == 8 and (i+1)%3 == 0 and (i+1) != 9:
+                boardString += "- - - - - - - - - - - \n"
+    print(boardString)
+
+def find_empty (board):
+    '''Finds an empty cell and returns its position as a tuple'''
+    for i in range (9):
+        for j in range (9):
+            if board[i][j] == 0:
+                return i,j
+
+def valid(board, pos, num):
+    '''Whether a number is valid in that cell, returns a bool'''
+    for i in range(9):
+        if board[i][pos[1]] == num and (i, pos[1]) != pos:  #make sure it isn't the same number we're checking for by comparing coords
+            return False
+
+    for j in range(9):
+        if board[pos[0]][j] == num and (pos[0], j) != pos:  #Same row but not same number
+            return False
+
+    start_i = pos[0] - pos[0] % 3 #ex. 5-5%3 = 3 and thats where the grid starts
+    start_j = pos[1] - pos[1] % 3
+    for i in range(3):
+        for j in range(3):  #adds i and j as needed to go from start of grid to where we need to be
+            if board[start_i + i][start_j + j] == num and (start_i + i, start_j + j) != pos:
+                return False
+    return True
+
+def solve(board):
+    '''Solves the Sudoku board via the backtracking algorithm'''
+    empty = find_empty(board)
+    if not empty: #no empty spots are left so the board is solved
+        return True
+
+    for nums in range(9):
+        if valid(board, empty,nums+1):
+            board[empty[0]][empty[1]] = nums+1
+
+            if solve(board): #recursive step
+                return True
+            board[empty[0]][empty[1]] = 0 #this number is wrong so we set it back to 0
+    return False
+
+if __name__ == '__main__':
+    board =  [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+    ch1 = 'y'
+    while(ch1 == 'y'):
+        
+        print("Enter Your Choice ")
+        print("1. Creating A Sudoku Board ")
+        print("2. Print A Board")
+        print("3. Check for Answer")
+        ch = int(input('[1/2/3] : '))
+        
+        if(ch == 1):
+            #Cathy Input Board
+            number = int(input("Enter the no of input numbers : "))
+            for i in range(number):
+                row = int(input('Enter Row No. to change :'))
+                column = int(input('Enter Column No. to change :'))
+                board[row][column] = int(input('Enter the new number : '))
+                print_board(board)
+        elif(ch == 2):
+            print_board(board)
+        else:
+            solve(board)
+            print_board(board)
+        
+        ch1 = input('Do you want to Continue [y/n] : ')
+    
